f8c90171ace29038b56a2045483ff9a2e20add4d
MD Abir Mahmud <abirmahmud5665@gmail.com>
2025-05-01 12:05:17 +0600

    done : basic folder structure

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,3 @@
+import { DashboardLayout } from "@/layouts";
+
+export default DashboardLayout;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+  return (
+    <main className="bg-[#F3F7FE] w-full h-screen flex justify-center items-center">
+      <div className="bg-white rounded-md shadow-md p-10 max-w-xl">
+        Welcome to login
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
