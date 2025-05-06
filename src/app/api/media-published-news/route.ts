fcc53feafae05c6b9b3b3a7ed55a1c58655bbdaf
MD Abir Mahmud <abirmahmud5665@gmail.com>
2025-05-06 14:36:44 +0600

    don : Media published news CURD opeations

--- src/app/api/media-published-news/route.ts
+++ src/app/api/media-published-news/route.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@/generated/prisma";
 import { ApiError, catchAsync, SuccessResponse } from "@/lib";
+import { paginationHelper } from "@/lib/paginationHelper/paginationHelper";
 import { TAccessTokenPayload } from "@/types";
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, unlink, writeFile } from "fs/promises";
 import _ from "lodash";
 import path from "path";
+import { z } from "zod";
 import { mediaPublishedNews } from "../../../lib/zod/mediaPublishedNews/mediaPublishedNews.schema";
 
 const prisma = new PrismaClient();
@@ -54,4 +56,70 @@ export const POST = catchAsync(
     return SuccessResponse(200, "Media published news created successfully!");
   },
   { checkAuth: true }
+);
+
+export const GET = catchAsync(
+  async (req: Request) => {
+    const { page, limit, skip } = paginationHelper(req.url);
+
+    const result = await prisma.mediaPublishedNews.findMany({
+      orderBy: { createdAt: "desc" },
+      include: {
+        createdBy: {
+          select: {
+            id: true,
+            fullName: true,
+          },
+        },
+      },
+      skip,
+      take: limit,
+    });
+
+    const count = await prisma.mediaPublishedNews.count();
+
+    return SuccessResponse(
+      200,
+      "Media published news retrieved successfully!",
+      { mediaPublishedNews: result },
+      undefined,
+      { page, limit, total: count }
+    );
+  },
+  { checkAuth: false }
+);
+export const DELETE = catchAsync(
+  async (req: Request) => {
+    const body = await req.json();
+
+    const { id } = z
+      .object({
+        id: z.string({ required_error: "Id is required!" }),
+      })
+      .parse(body);
+
+    const existingNews = await prisma.mediaPublishedNews.findUnique({
+      where: { id },
+    });
+    if (!existingNews) throw new ApiError(400, "No news found!");
+
+    if (existingNews.thumb) {
+      const oldThumbPath = path.join(
+        process.cwd(),
+        "public",
+        existingNews.thumb
+      );
+      try {
+        await unlink(oldThumbPath);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.warn("Failed to delete old thumb:", err); // Log but don't throw
+      }
+    }
+
+    await prisma.mediaPublishedNews.delete({ where: { id } });
+
+    return SuccessResponse(200, "Media published news deleted successfully!");
+  },
+  { checkAuth: false }
 );
--- src/lib/paginationHelper/paginationHelper.ts
+++ src/lib/paginationHelper/paginationHelper.ts
@@ -1,7 +1,7 @@
 export const paginationHelper = (url: string) => {
   const { searchParams } = new URL(url);
   const limit = Number(searchParams.get("limit")) || 12;
-  const page = Number(searchParams.get("page")) || 0;
+  const page = Number(searchParams.get("page")) || 1;
 
   return {
     limit,
